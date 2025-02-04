22de4f8e8efbb66eb6c5b11ec28218b90a9a163f
sdfg6213 <sdfg6213@gmail.com>
2025-02-04 15:14:17 +0900

    매출v1.1

--- semiProject2/src/semiProject2/Sales.java
+++ semiProject2/src/semiProject2/Sales.java
@@ -12,10 +12,11 @@ public class Sales extends Frame implements ActionListener{
 	static Connection con;
 	Panel p_center,p_south,p_salesInput,p_salesDelete,p_salesCheck,p_salesInfo;
 	Button bt1,bt2,bt3,bt4,bt_salesInput,bt_salesDelete,bt_mainView,bt_salesCheck;
-	TextField tf_sidate, tf_sidept, tf_expectincome,tf_actualincome,tf_outcome,tf_sddate,tf_sddept;
-	String date_str,msg,info_date,info_dept,info_expectincome,info_actualincome,info_outcome,info_achiev;
-	Choice sidate_c,sddate_c;
+	TextField tf_sidate, tf_sidept, tf_expectincome,tf_actualincome,tf_teamcost,tf_sddate,tf_sddept,tf_scdept;
+	String date_str,msg,info_date,info_dept,info_expectincome,info_actualincome,info_teamcost,info_achiev;
+	Choice sidate_c,sddate_c,scdate_c;
 	Label msg_label;
+	int dept,cumincome,actualIncome;
 	
 	public Sales() {
 		BorderLayout bl = new BorderLayout(20,20);
@@ -62,6 +63,8 @@ public class Sales extends Frame implements ActionListener{
 		}else if(obj==bt_salesInput) {
 			date_str = sidate_c.getSelectedItem();
 			this.salesInput(con);
+			this.cumincomeCheck(con);
+			this.cumincomeUpdate(con);
 			this.validate();
 		}else if(obj==bt2) {
 			this.remove(p_center);
@@ -75,22 +78,28 @@ public class Sales extends Frame implements ActionListener{
 		}else if(obj==bt_mainView) {
 			dispose();
 			Sales s = new Sales();
-			s.setSize(300,300);
+			s.setSize(300,400);
 			s.setVisible(true);
 		}else if(obj==bt_salesDelete) {
 			date_str = sddate_c.getSelectedItem();
 			this.salesDelete(con);
+			this.validate();
 			
 		}else if(obj==bt3) {
 			this.remove(p_center);
 			this.southViewDeletion();
 			this.salesCheckView();
+			System.out.println("hi hello");
 			this.add(p_salesCheck,"Center");
 			this.validate();
 		}else if(obj==bt_salesCheck) {
+			date_str = scdate_c.getSelectedItem();
 			this.salesView(con);
+			System.out.println("hi hello");
 			this.remove(p_salesCheck);
 			this.salesInfoView();
+			this.add(p_salesInfo,"Center");
+			this.validate();
 			
 		}
 	}
@@ -104,12 +113,12 @@ public class Sales extends Frame implements ActionListener{
 	}
 	
 	public void salesInputView() {
-		p_salesInput = new Panel(new GridLayout(6,2,10,10));
+		p_salesInput = new Panel(new GridLayout(10,1,10,10));
 		tf_sidate = new TextField();
 		tf_sidept = new TextField();
 		tf_expectincome = new TextField();
 		tf_actualincome = new TextField();
-		tf_outcome = new TextField();
+		tf_teamcost = new TextField();
 //		tf_date = new TextField();
 		p_salesInput.add(new Label("날짜 입력"));
 		sidate_c = new Choice();
@@ -121,22 +130,17 @@ public class Sales extends Frame implements ActionListener{
 		
 		p_salesInput.add(new Label("부서번호 입력"));
 		p_salesInput.add(tf_sidept);
-		tf_sidept.getText();
-		p_salesInput.add(new Label("예상 매출 입력"));
-		p_salesInput.add(tf_expectincome);
 		p_salesInput.add(new Label("실제 매출 입력"));
 		p_salesInput.add(tf_actualincome);
 		p_salesInput.add(new Label("비용 입력"));
-		p_salesInput.add(tf_outcome);
-		bt_salesInput = new Button("입력");
-		msg_label = new Label(msg);
-		p_salesInput.add(msg_label);
+		p_salesInput.add(tf_teamcost);
+		bt_salesInput = new Button("정보 입력");
 		p_salesInput.add(bt_salesInput);
 		bt_salesInput.addActionListener(this);
 
 	}
 	public void salesDeleteView() {
-		p_salesDelete = new Panel(new GridLayout(5,1));
+		p_salesDelete = new Panel(new GridLayout(6,1));
 		tf_sddept = new TextField();
 		sddate_c = new Choice();
 		System.out.println("hi");
@@ -157,19 +161,18 @@ public class Sales extends Frame implements ActionListener{
 	}
 	public void salesCheckView() {
 		p_salesCheck= new Panel(new GridLayout(5,1));
-		tf_sddept = new TextField();
-		sddate_c = new Choice();
-		tf_sddept = new TextField();
+		scdate_c = new Choice();
+		tf_scdept = new TextField();
 		bt_salesCheck = new Button("조회 정보 입력");
 		System.out.println("hi");
-		sddate_c.addItem("2024-01");
-		sddate_c.addItem("2024-02");
-		sddate_c.addItem("2024-03");
-		sddate_c.addItem("2024-04");
+		scdate_c.addItem("2024-01");
+		scdate_c.addItem("2024-02");
+		scdate_c.addItem("2024-03");
+		scdate_c.addItem("2024-04");
 		p_salesCheck.add(new Label("날짜:"));
-		p_salesCheck.add(sddate_c);
+		p_salesCheck.add(scdate_c);
 		p_salesCheck.add(new Label("부서번호:"));
-		p_salesCheck.add(tf_sddept);
+		p_salesCheck.add(tf_scdept);
 		p_salesCheck.add(bt_salesCheck);
 		bt_salesCheck.addActionListener(this);
 		
@@ -179,79 +182,110 @@ public class Sales extends Frame implements ActionListener{
 //		p_salesCheck.add(new Label("달성도:"));
 	}
 	public void salesInfoView() {
-		p_salesInfo = new Panel(new GridLayout(6,2));
+		p_salesInfo = new Panel(new GridLayout(8,1));
+		
 		p_salesInfo.add(new Label("날짜:"));
 		p_salesInfo.add(new Label(info_date));
 		p_salesInfo.add(new Label("부서번호:"));
 		p_salesInfo.add(new Label(info_dept));
-		p_salesInfo.add(new Label("예상매출:"));
-		p_salesInfo.add(new Label(info_expectincome));
 		p_salesInfo.add(new Label("실제매출:"));
 		p_salesInfo.add(new Label(info_actualincome));
 		p_salesInfo.add(new Label("비용:"));
-		p_salesInfo.add(new Label(info_outcome));
-		p_salesInfo.add(new Label("달성도:"));
-		p_salesInfo.add(new Label(info_achiev));
-		
+		p_salesInfo.add(new Label(info_teamcost));
 	}
 	
 	public void salesInput(Connection con) {
-			String sql = "insert into income values(TO_DATE('"+date_str+"', 'YYYY-MM'),?,?,?,?,?)";
+			String sql = "insert into income values('"+date_str+"',?,?,?)";
+			String sql_teamCumincomeCheck = "select cumincome from team where dept=?";
+			String sql_teamCumincomeUpdate = "update team set cumincome=? where dept=?";
 			PreparedStatement ps;
+			PreparedStatement ps2;
+			PreparedStatement ps3;
 			try {
 				ps = con.prepareStatement(sql);
-				int dept=Integer.parseInt(tf_sidept.getText());
+				dept=Integer.parseInt(tf_sidept.getText());
 				ps.setInt(1, dept );
-				int expectIncome=Integer.parseInt(tf_expectincome.getText());
-				ps.setInt(2, expectIncome );
-				int actualIncome=Integer.parseInt(tf_actualincome.getText());
-				ps.setInt(3, actualIncome);
-				int outcome=Integer.parseInt(tf_outcome.getText());
-				ps.setInt(4, outcome);
-				ps.setString(5, actualIncome*100/expectIncome+"%");
+				actualIncome=Integer.parseInt(tf_actualincome.getText());
+				ps.setInt(2, actualIncome);
+				System.out.println("actualIncome:"+actualIncome);
+				int teamcost=Integer.parseInt(tf_teamcost.getText());
+				ps.setInt(3, teamcost);
 				int count = ps.executeUpdate();
-				msg =count + "개의 정보가 입력됨";
-				msg_label = new Label(msg);
-				System.out.println(count + "개의 정보가 입력됨");
+				p_salesInput.add(new Label(count + "개의 정보가 입력됨"));
+				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-//			YearMonth ym = YearMonth.of(year,month);
-//			Date sqlDate = Date.valueOf(ym.atDay(0));
-//			ps.setDate(1, sqlDate);
-//			ps.setString(1, year+"-"+month);
 	}
+	
+	public void cumincomeCheck(Connection con) {
+		String sql_teamCumincomeCheck = "select cumincome from team where dept=?";
+		PreparedStatement ps2;
+		try {
+			ps2 = con.prepareStatement(sql_teamCumincomeCheck);
+			ps2.setInt(1, dept);
+			ResultSet rs = ps2.executeQuery();
+			while(rs.next()) {
+				cumincome = rs.getInt(1);
+				System.out.println("rs.getInt(1):"+rs.getInt(1));
+				System.out.println("cumincome:"+cumincome);
+			}
+			System.out.println("cumincome:"+cumincome);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void cumincomeUpdate(Connection con) {
+		String sql_teamCumincomeUpdate = "update team set cumincome=? where dept=?";
+		PreparedStatement ps3;
+		try {
+			ps3 = con.prepareStatement(sql_teamCumincomeUpdate);
+			ps3.setInt(1, cumincome+actualIncome);
+			ps3.setInt(2, dept);
+			System.out.println("업데이트정보:"+(cumincome+actualIncome));
+			int count2 = ps3.executeUpdate();
+			System.out.println(count2+"개의 정보 수정");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void salesDelete(Connection con) {
-		String sql = "delete from income where mon_income=TO_DATE('"+date_str+"', 'YYYY-MM') and dept =?";
+		String sql = "delete from income where mon_income='"+date_str+"' and dept =?";
 		PreparedStatement ps;
 		try {
 			ps = con.prepareStatement(sql);
 			int dept=Integer.parseInt(tf_sddept.getText());
 			ps.setInt(1, dept);
 			int count = ps.executeUpdate();
-			System.out.println(count+"개의 정보가 삭제됨");
+			p_salesDelete.add(new Label(count + "개의 정보가 삭제됨"));
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	public void salesView(Connection con) {
-		String sql="select * from income where mon_income=TO_DATE('"+date_str+"', 'YYYY-MM') and dept =?";
+		String sql="select * from income where mon_income=? and dept =?";
 		PreparedStatement ps;
 		try {
 			ps = con.prepareStatement(sql);
-			int dept=Integer.parseInt(tf_sddept.getText());
-			ps.setInt(1, dept);
+			int dept=Integer.parseInt(tf_scdept.getText());
+			ps.setString(1, date_str);
+			ps.setInt(2, dept);
+			System.out.println(sql);
 			ResultSet rs = ps.executeQuery();
-			System.out.println("hi");
+			
 			while(rs.next()) {
-				info_date = rs.getDate(1).toString();
+				info_date = rs.getString(1);
 				info_dept = Integer.toString(rs.getInt(2));
-				info_expectincome = Integer.toString(rs.getInt(3));
-				info_actualincome = Integer.toString(rs.getInt(4));
-				info_outcome = Integer.toString(rs.getInt(5));
-				info_achiev = Integer.toString(rs.getInt(6));
+				info_actualincome = Integer.toString(rs.getInt(3));
+				info_teamcost = Integer.toString(rs.getInt(4));
 				
 			}
 			
@@ -261,6 +295,7 @@ public class Sales extends Frame implements ActionListener{
 		}
 		
 	}
+	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -276,7 +311,7 @@ public class Sales extends Frame implements ActionListener{
 			e.printStackTrace();
 		}
 		Sales s =new Sales();
-		s.setSize(300,300);
+		s.setSize(300,400);
 		s.setVisible(true);
 		
 		
