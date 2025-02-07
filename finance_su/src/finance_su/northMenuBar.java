0f792bf0715a6c35f333c8cca8511df4b4c81e08
User <User@LAPTOP-T51IHN12>
2025-02-07 11:42:06 +0900

    상단바생성v1.0으로 올립니다.

---
+++ finance_su/src/finance_su/northMenuBar.java
@@ -0,0 +1,62 @@
+package finance_su;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class northMenuBar extends JFrame {
+
+	JPanel northFirstP;
+	JButton b_menusave, b_menufavorites, b_menuclose;
+
+	public northMenuBar() {
+		super();
+
+		BorderLayout mainLayout = new BorderLayout(10, 10);
+		this.setLayout(mainLayout);
+
+		northFirstP = new JPanel();
+		FlowLayout northLayout = new FlowLayout(FlowLayout.LEFT, 15, 15);
+		northFirstP.setLayout(northLayout);
+		northFirstP.setBackground(Color.black);
+
+		b_menusave = new JButton("Áñ°ÜÃ£±â");
+		this.l_menu_design(b_menusave);
+		this.jb_color_change(b_menusave);
+
+		b_menufavorites = new JButton("µÚ·Î°¡±â");
+		this.l_menu_design(b_menufavorites);
+		this.jb_color_change(b_menufavorites);
+
+		b_menuclose = new JButton("´Ý±â");
+		this.l_menu_design(b_menuclose);
+		this.jb_color_change(b_menuclose);
+
+		northFirstP.add(b_menusave);
+		northFirstP.add(b_menufavorites);
+		northFirstP.add(b_menuclose);
+
+		this.add(northFirstP, "North");
+
+		this.setSize(500, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+
+	public void jb_color_change(JButton jb) {
+		jb.setBackground(Color.black);
+		jb.setForeground(Color.white);
+	}
+
+	public void l_menu_design(JButton jl) {
+		jl.setForeground(Color.white);
+		jl.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+	}
+
+	public static void main(String[] args) {
+
+		new northMenuBar();
+
+	}
+
+}
