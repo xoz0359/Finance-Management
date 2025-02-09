ff6a03c5f34cbbbb64fb2f03e5054a6c316b7554
soi-fafa <okere313@gmail.com>
2025-02-09 16:29:44 +0900

    JPanel 상속 내에서 메서드 실행 가능

--- inserrDept/src/inserrDept/InsertDept.java
+++ inserrDept/src/inserrDept/InsertDept.java
@@ -13,8 +13,7 @@ public class InsertDept extends JPanel {
 
     public InsertDept() {
     	gridbag = new GridBagLayout();
-    	getContentPane().setLayout(gridbag);
-    	setTitle("사용자 등록");
+
     	
     	constraint = new GridBagConstraints();
     	constraint.fill = GridBagConstraints.BOTH;
@@ -24,25 +23,25 @@ public class InsertDept extends JPanel {
         constraint.gridy = 0;
         deptCode = new JLabel("계정코드명  :", JLabel.RIGHT);
         gridbag.setConstraints(deptCode, constraint);
-        getContentPane().add(deptCode);
+        add(deptCode);
         
         constraint.gridx = 1;
         constraint.gridy = 0;
         codeNumber = new JTextField(10);
         gridbag.setConstraints(codeNumber, constraint);
-        getContentPane().add(codeNumber);
+        add(codeNumber);
         
         constraint.gridx = 2;
         constraint.gridy = 0;
         l1 = new JLabel();
         gridbag.setConstraints(l1, constraint);
-        getContentPane().add(l1);
+        add(l1);
         
         constraint.gridx = 0;
         constraint.gridy = 1;
         l2 = new JLabel();
         gridbag.setConstraints(l2, constraint);
-        getContentPane().add(l2);
+        add(l2);
         
         constraint.fill = GridBagConstraints.NONE;
         constraint.anchor = GridBagConstraints.EAST;
@@ -51,24 +50,16 @@ public class InsertDept extends JPanel {
         jbttnSave = new JButton("저장");
         jbttnSave.setPreferredSize(new Dimension(60, 23));
         gridbag.setConstraints(jbttnSave, constraint);
-        getContentPane().add(jbttnSave);
+        add(jbttnSave);
         
         constraint.gridx = 2;
         constraint.gridy = 1;
         jbttnClose = new JButton("닫기");
         jbttnClose.setPreferredSize(new Dimension(60, 23));
         gridbag.setConstraints(jbttnClose, constraint);
-        getContentPane().add(jbttnClose);
+        add(jbttnClose);
         
-        
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setResizable(false);
         setBounds(1000, 200, 450, 250);
-    }
-    
-    public static void main(String[] args) {
-    	new InsertDept();
-    	System.out.println("asdasdasd");
     }
 }
