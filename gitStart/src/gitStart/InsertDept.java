20aabbf4fde3ea9eb8def8c82dd750a511b04e92
soi <okere313@gmail.com>
2025-02-10 15:19:44 +0900

    부서이름 입력 Utf-8 -> ms949로 변경

---
+++ gitStart/src/gitStart/InsertDept.java
@@ -0,0 +1,71 @@
+package gitStart;
+
+import java.awt.*;
+import javax.swing.*;
+
+
+public class InsertDept extends JFrame {
+	JButton jbttnSave,jbttnClose;
+	JLabel deptCode,l1,l2,l3,l4,l5,l6;
+	JTextField codeNumber;
+    GridBagConstraints constraint;
+    GridBagLayout gridbag;
+
+    public InsertDept() {
+    	gridbag = new GridBagLayout();
+	setLayout(gridbag);
+    	
+    	constraint = new GridBagConstraints();
+    	constraint.fill = GridBagConstraints.BOTH;
+    	constraint.insets = new Insets(0, 0, 20, 10);
+
+    	constraint.gridx = 0;
+        constraint.gridy = 0;
+        deptCode = new JLabel("부서이름  :", JLabel.RIGHT);
+        gridbag.setConstraints(deptCode, constraint);
+        add(deptCode);
+        
+        constraint.gridx = 1;
+        constraint.gridy = 0;
+        codeNumber = new JTextField(10);
+        gridbag.setConstraints(codeNumber, constraint);
+        add(codeNumber);
+        
+        constraint.gridx = 2;
+        constraint.gridy = 0;
+        l1 = new JLabel();
+        gridbag.setConstraints(l1, constraint);
+        add(l1);
+        
+        constraint.gridx = 0;
+        constraint.gridy = 1;
+        l2 = new JLabel();
+        gridbag.setConstraints(l2, constraint);
+        add(l2);
+        
+        constraint.fill = GridBagConstraints.NONE;
+        constraint.anchor = GridBagConstraints.EAST;
+        constraint.gridx = 1;
+        constraint.gridy = 1;
+        jbttnSave = new JButton("저장");
+        jbttnSave.setPreferredSize(new Dimension(60, 23));
+        gridbag.setConstraints(jbttnSave, constraint);
+        add(jbttnSave);
+        
+        constraint.gridx = 2;
+        constraint.gridy = 1;
+        jbttnClose = new JButton("닫기");
+        jbttnClose.setPreferredSize(new Dimension(60, 23));
+        gridbag.setConstraints(jbttnClose, constraint);
+        add(jbttnClose);
+        
+        setVisible(true);
+        setBounds(1000, 200, 450, 250);
+    }
+    public static void main(String[] args) {
+    	new InsertDept();
+    }
+}
+
+
+
