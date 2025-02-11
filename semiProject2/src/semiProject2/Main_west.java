f5ea3c87550f591094e9e3f4018b8c4ee1867a0a
sdfg6213 <102571693+sdfg6213@users.noreply.github.com>
2025-02-11 09:44:19 +0900

    Main_west랑 NorthMenubar 아이콘추가되도록 업데이트 + 다른 파일 추가
    아이콘추가할 메서드 담긴 ImageIconAdd 파일이랑 각 페이지에 타이틀 담을 TitleSet 파일 추가

--- semiProject2/src/semiProject2/Main_west.java
+++ semiProject2/src/semiProject2/Main_west.java
@@ -1,48 +1,61 @@
-package semiProject2;
+package finance_Management;
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Main_west extends JFrame{
+public class Main_west extends JPanel{
 	
 	Container cont1;
 	JPanel p_main_west,p_main_center;
 	JLabel title,l_menu1,l_menu2,l_menu3,l_menu4,l_menu5;
 	JMenuBar jmb;
 	JButton jb_stateInput,
-			jb_stateSelect,jb_teamInput,jb_teamSelect,
+			jb_stateSelect,jb_teamInput,jb_teamSelect,jb_teamExpectIncomeSet,
 			jb_incomeSelect,jb_incomeAnalysis,jb_fin_st,
 			jb_userRegiser,jb_codeRegiser;
 	
 	
 	public Main_west() {
+		String path = "src\\finIcon.png";
+		ImageIconAdd iconAdd = new ImageIconAdd(path, 20,20);
+
 		//센터판넬과 서쪽판넬 생성
-		p_main_west = new JPanel(new GridLayout(13,1));
+		p_main_west = new JPanel(new GridLayout(14,1));
 		p_main_center = new JPanel(new GridLayout(1,1));
 		
 		//메인 화면에 센터판넬에 들어갈 라벨 생성
-		title = new JLabel("재무 관리 프로그램v1.0",JLabel.CENTER);
+		title = new JLabel("재무 관리 프로그램v1.0",iconAdd.icon,JLabel.CENTER);
+		
+//		title.setText("재무");
+//		title.setIcon(icon);
 		
 		//센터판넬에 라벨 추가
 		p_main_center.add(title);
 		
-		//JFrame가장 바깥 pane인 컨텐트페인을 담을 객체 선언하고 대입
-		Container cont=getContentPane();
-		
 		//위에서 생성했던 컨텐트페인 담긴 객체 cont에 서쪽판넬이랑 중심판넬 추가
-		cont.add(p_main_west,"West");
-		cont.add(p_main_center,"Center");
+		//this.add(p_main_west,"West");
+		this.add(p_main_center,"Center");
 		
 		//서쪽판넬 배경 검은색
-		p_main_west.setBackground(Color.black);
-
+		p_main_west.setBackground(Color.decode("#3F91D5"));
+		
+		String pathState = "src\\img\\wirte.png";
+		String pathTeam = "src\\img\\department.png";
+		String pathIncome = "src\\img\\income2.png";
+		String pathSetting = "src\\img\\setting4.png";
+		
+		ImageIconAdd IconState = new ImageIconAdd(pathState, 20, 20);
+		ImageIconAdd IconTeam = new ImageIconAdd(pathTeam, 20, 20);
+		ImageIconAdd IconIncome = new ImageIconAdd(pathIncome, 20, 20);
+		ImageIconAdd IconSetting = new ImageIconAdd(pathSetting, 20, 20);
+		
 		//큰 메뉴 객체 생성 & 디자인 메서드 사용
-		l_menu1 = new JLabel("회계관리");this.l_menu_design(l_menu1);
-		l_menu2 = new JLabel("부서관리");this.l_menu_design(l_menu2);
-		l_menu3 = new JLabel("매출정보");this.l_menu_design(l_menu3);
+		l_menu1 = new JLabel("회계관리",IconState.icon,JLabel.CENTER);this.l_menu_design(l_menu1);
+		l_menu2 = new JLabel("부서관리",IconTeam.icon,JLabel.CENTER);this.l_menu_design(l_menu2);
+		l_menu3 = new JLabel("매출정보",IconIncome.icon,JLabel.CENTER);this.l_menu_design(l_menu3);
 //		l_menu4 = new JLabel("재무제표");this.l_menu_design(l_menu4);
-		l_menu5 = new JLabel("*환경설정");this.l_menu_design(l_menu5);
+		l_menu5 = new JLabel("환경설정",IconSetting.icon,JLabel.CENTER);this.l_menu_design(l_menu5);
 		
 		//회계 관련 작은메뉴버튼 생성 & 디자인
 		jb_stateInput=new JButton("회계정보 입력");jb_stateSelect=new JButton("회계정보 조회");
@@ -50,10 +63,12 @@ public class Main_west extends JFrame{
 		
 		//부서 관련 작은메뉴버튼 생성 & 디자인
 		jb_teamInput = new JButton("부서정보 입력"); jb_teamSelect=new JButton("부서정보 조회");
+		jb_teamExpectIncomeSet = new JButton("예상매출 설정");
 		this.jb_color_change(jb_teamInput);this.jb_color_change(jb_teamSelect);
+		this.jb_color_change(jb_teamExpectIncomeSet);
 		
 		//매출 관련 작은메뉴버튼 생성 & 디자인
-		jb_incomeSelect = new JButton("매출정보 입력"); jb_incomeAnalysis=new JButton("매출 분석");
+		jb_incomeSelect = new JButton("매출정보 조회"); jb_incomeAnalysis=new JButton("매출 분석");
 		this.jb_color_change(jb_incomeSelect);this.jb_color_change(jb_incomeAnalysis);
 		
 		//환경설정 관련 메뉴버튼 생성 & 디자인
@@ -71,7 +86,7 @@ public class Main_west extends JFrame{
 		p_main_west.add(jb_stateInput);p_main_west.add(jb_stateSelect);
 		
 		p_main_west.add(l_menu2);//부서관리 메뉴 + 버튼
-		p_main_west.add(jb_teamInput); p_main_west.add(jb_teamSelect);
+		p_main_west.add(jb_teamInput); p_main_west.add(jb_teamSelect);p_main_west.add(jb_teamExpectIncomeSet);
 		
 		p_main_west.add(l_menu3);//매출정보 메뉴 + 버튼
 		p_main_west.add(jb_incomeSelect); p_main_west.add(jb_incomeAnalysis);
@@ -95,7 +110,7 @@ public class Main_west extends JFrame{
 	
 	//버튼 색 바꾸기
 	public void jb_color_change(JButton jb) {
-		jb.setBackground(Color.black);jb.setForeground(Color.white);
+		jb.setBackground(Color.decode("#3F91D5"));jb.setForeground(Color.white);
 	}
 	//큰 메뉴 폰트 바꾸기
 	public void l_menu_design(JLabel jl) {
@@ -103,16 +118,4 @@ public class Main_west extends JFrame{
 		jl.setFont(new Font("맑은 고딕",Font.BOLD,20));
 		jl.setHorizontalAlignment(JLabel.CENTER);
 	}
-	
-
-	public static void main(String[] args) {
-		Main_west mw =new Main_west();
-		mw.setSize(500,600);
-		mw.setVisible(true);
-		mw.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//		mw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-
-	}
-
 }
---
+++ semiProject2/src/semiProject2/NorthMenuBar.java
@@ -0,0 +1,74 @@
+package finance_Management;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class NorthMenuBar extends JPanel {
+
+	JPanel northFirstP;
+	JButton b_menufavorites, b_forward, b_backward, b_menuclose;
+
+	public NorthMenuBar() {
+		super();
+
+		BorderLayout mainLayout = new BorderLayout(10, 10);
+		this.setLayout(mainLayout);
+
+		northFirstP = new JPanel();
+		FlowLayout northLayout = new FlowLayout(FlowLayout.LEFT, 15, 15);
+		northFirstP.setLayout(northLayout);
+		northFirstP.setBackground(Color.decode("#1879C9"));
+		
+		//¾ÆÀÌÄÜ»ý¼º
+		String backwardPath = "src\\img\\backward.png";
+		String forwardPath = "src\\img\\forward.png";
+		String starPath = "src\\img\\star.png";
+		String closePath = "src\\img\\close.png";
+		ImageIconAdd backwardIcon = new ImageIconAdd(backwardPath, 20, 20);
+		ImageIconAdd forwardIcon = new ImageIconAdd(forwardPath, 20, 20);
+		ImageIconAdd starIcon = new ImageIconAdd(starPath, 20, 20);
+		ImageIconAdd closeIcon = new ImageIconAdd(closePath, 20, 20);
+
+		b_backward = new JButton(backwardIcon.icon);
+		this.l_menu_design(b_backward);
+		this.jb_color_change(b_backward);
+		
+		b_forward = new JButton(forwardIcon.icon);
+		this.l_menu_design(b_forward);
+		this.jb_color_change(b_forward);
+		
+		b_menufavorites = new JButton("Áñ°ÜÃ£±â",starIcon.icon);
+		this.l_menu_design(b_menufavorites);
+		this.jb_color_change(b_menufavorites);
+
+		b_menuclose = new JButton("´Ý±â",closeIcon.icon);
+		this.l_menu_design(b_menuclose);
+		this.jb_color_change(b_menuclose);
+
+		northFirstP.add(b_backward);
+		northFirstP.add(b_forward);
+		northFirstP.add(b_menufavorites);
+		northFirstP.add(b_menuclose);
+
+		this.add(northFirstP, "Center");
+		String iconPath = "src\\img\\setting4.png";
+		ImageIconAdd iconAdd = new ImageIconAdd(iconPath, 50, 50);
+		JButton jb_icon = new JButton(iconAdd.icon);
+		this.add(jb_icon, "East");
+		jb_icon.setBackground(Color.decode("#1879C9"));
+		this.setBackground(Color.decode("#1879C9"));
+
+	}
+
+	public void jb_color_change(JButton jb) {
+		jb.setBackground(Color.decode("#1879C9"));
+		jb.setForeground(Color.white);
+	}
+
+	public void l_menu_design(JButton jl) {
+		jl.setForeground(Color.white);
+		jl.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+	}
+
+
+}
